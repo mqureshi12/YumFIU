4fe91f0626aab276e9b8827ab289a3b0c00deb19
Daniel <danielortiz6810@gmail.com>
2023-10-05 20:45:32 -0400

    finished MenuItems.js

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { View, Text} from "react-native"
+import { View, Text } from "react-native";
 import { Divider } from "react-native-elements";
 import About from "../components/restaurantDetail/About";
+import MenuItems from "../components/restaurantDetail/MenuItems";
 
 export default function RestaurantDetail() {
-    return (
-        <View>
-            <About />
-            <Divider width={1.8} style={{ marginVertical: 20 }} />
-        </View>
-    );
+  return (
+    <View>
+      <About />
+      <Divider width={1.8} style={{ marginVertical: 20 }} />
+      <MenuItems />
+    </View>
+  );
 }
